2843a1b9f0ca956681096a3d2e0e7eab0c101469
alka <almedin.alikadic@edu.fit.ba>
2021-05-28 23:31:13 +0200

    Added Mobile/Xamarin project

---
+++ eHousing/eHousing.Mobile/eHousing.Mobile/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using eHousing.Mobile.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace eHousing.Mobile.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
---
+++ eHousing/eHousing.Mobile/eHousing.Mobile/Views/ItemsPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using eHousing.Mobile.Models;
+using eHousing.Mobile.ViewModels;
+using eHousing.Mobile.Views;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace eHousing.Mobile.Views
+{
+    public partial class ItemsPage : ContentPage
+    {
+        ItemsViewModel _viewModel;
+
+        public ItemsPage()
+        {
+            InitializeComponent();
+
+            BindingContext = _viewModel = new ItemsViewModel();
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            _viewModel.OnAppearing();
+        }
+    }
+}
